8da72706f604384ccf6e6fd9dd3b94996652f4d9
bulat <bulat.93@inbox.ru>
2019-03-24 22:37:45 +0300

    Implemented JPA Visit Service, added loader data. Closes #46

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/map/VisitMapService.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/map/VisitMapService.java
@@ -2,9 +2,11 @@ package guru.springframework.sfgpetclinic.services.map;
 
 import guru.springframework.sfgpetclinic.model.Visit;
 import guru.springframework.sfgpetclinic.services.VisitService;
+import org.springframework.stereotype.Service;
 
 import java.util.Set;
 
+@Service
 public class VisitMapService extends AbstractMapService<Visit, Long> implements VisitService {
 
     @Override
@@ -24,10 +26,11 @@ public class VisitMapService extends AbstractMapService<Visit, Long> implements 
 
     @Override
     public Visit save(Visit visit) {
-        if (visit.getPet() == null || visit.getPet().getOwner() == null || visit.getPet().getId() == null
-                || visit.getPet().getOwner().getId() == null) {
+        if(visit.getPet() == null || visit.getPet().getOwner() == null || visit.getPet().getId() == null
+                || visit.getPet().getOwner().getId() == null){
             throw new RuntimeException("Invalid Visit");
         }
+
         return super.save(visit);
     }
 
